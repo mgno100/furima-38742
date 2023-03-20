48dbe1f1ab41016f9899fa284177678091c16637
mgno100 <tls.krl.psk.tnm@gmail.com>
2023-03-20 10:24:43 +0900

    バリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください'}
+    validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください'}
+    validates :last_name_kana, format: {with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナ文字を使用してください'}
+    validates :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナ文字を使用してください'}
+    validates :birthday
+  end
+      
+  validates :password, :encrypted_password, format: {with: /\A[a-zA-Z0-9]+\z/}
 end
