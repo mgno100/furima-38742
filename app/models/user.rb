bdbd4d266e36da83b9d2c70e9a9decc121c118ff
mgno100 <tls.krl.psk.tnm@gmail.com>
2023-03-20 15:40:00 +0900

    バリデーション設定の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,13 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください'}
-    validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください'}
-    validates :last_name_kana, format: {with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナ文字を使用してください'}
-    validates :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナ文字を使用してください'}
+    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー]+\z/ }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/ }
     validates :birthday
   end
-      
-  validates :password, :encrypted_password, format: {with: /\A[a-zA-Z0-9]+\z/}
+  
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX } 
 end
