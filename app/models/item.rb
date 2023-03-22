fc3c50fe02145d1b07fd89dd2a86f8bd2adae695
mgno100 <tls.krl.psk.tnm@gmail.com>
2023-03-22 19:00:26 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,23 @@ class Item < ApplicationRecord
   belongs_to :shipping_fee_status
   belongs_to :prefecture
   belongs_to :scheduled_delivery
+
+  with_options presence: true do
+    validates :image
+    validates :item_name
+    validates :item_info
+    validates :item_price
+  end
+  
+  with_options presence: true, numericality: { other_than: 1 , message: 'Select' } do
+    validates :category_id
+    validates :sales_status_id
+    validates :shipping_fee_status_id
+    validates :prefecture_id
+    validates :scheduled_delivery_id
+  end
+
+  validates :item_price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'is out of setting range' }
+  validates :item_price, numericality: { only_intger: true, message: 'is invalid. Input half-width characters' }
+  
 end
