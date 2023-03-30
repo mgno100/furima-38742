d1c8ce9cc00456101c6b4afe3dd97fa9bc256e87
mgno100 <tls.krl.psk.tnm@gmail.com>
2023-03-30 23:31:44 +0900

    ordersコントローラーにFormオブジェクトのインスタンスを生成するように記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,4 +3,20 @@ class OrdersController < ApplicationController
   def index
     @order_residence = OrderResidence.new
   end
+
+  def create
+    @order_residence = OrderResidence.new(order_params)
+    if @order_residence.valid?
+      @order_residence.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_residence).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 end
