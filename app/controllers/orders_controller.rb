6df0c17c4384c448591e8de8c0c718a6eb5bb347
mgno100 <tls.krl.psk.tnm@gmail.com>
2023-04-03 14:44:23 +0900

    ordersコントローラーに決済処理を記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,9 +6,9 @@ class OrdersController < ApplicationController
   end
 
   def create
-    binding.pry
     @order_residence = OrderResidence.new(order_params)
     if @order_residence.valid?
+      pay_item
       @order_residence.save
       redirect_to root_path
     else
@@ -20,5 +20,14 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order_residence).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp::api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.item_price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
